141aa485735d084e78db4785d2ecaafc20d5aed7
yun <yunyunpp96@outlook.com>
2024-03-09 23:53:29 +0800

    add test for `parse_envvar`

--- tests/testthat/test-envvars.R
+++ tests/testthat/test-envvars.R
@@ -1,4 +1,4 @@
-testthat::test_that("with_envvar works well", {
+testthat::test_that("with_envvar() works well", {
     testthat::expect_identical(
         with_envvar(c("TEMP_SECRET" = "secret"), Sys.getenv("TEMP_SECRET")),
         "secret"
@@ -6,5 +6,53 @@ testthat::test_that("with_envvar works well", {
     testthat::expect_identical(
         with_envvar(c(PATH = NA), Sys.getenv("PATH", unset = "")),
         ""
+    )
+})
+
+testthat::test_that("parse_envvar() works well", {
+    # one item ---------------------
+    testthat::expect_identical(
+        system2("echo", "$TEST",
+            env = parse_envvar(c(TEST = shQuote("I'm from parse_envvar()"))),
+            stdout = TRUE
+        ),
+        "I'm from parse_envvar()"
+    )
+    # multiple items ---------------------
+    testthat::expect_identical(
+        system2("echo", "$FIRST",
+            env = parse_envvar(
+                c(
+                    FIRST = shQuote("I'm the first"),
+                    SECOND = shQuote("I'm the second")
+                )
+            ),
+            stdout = TRUE
+        ),
+        "I'm the first"
+    )
+    testthat::expect_identical(
+        system2("echo", "$SECOND",
+            env = parse_envvar(
+                c(
+                    FIRST = shQuote("I'm the first"),
+                    SECOND = shQuote("I'm the second")
+                )
+            ),
+            stdout = TRUE
+        ),
+        "I'm the second"
+    )
+    testthat::expect_identical(
+        system2("echo", c("$FIRST", "$SECOND"),
+            env = parse_envvar(
+                c(
+                    FIRST = shQuote("I'm the first"),
+                    SECOND = shQuote("I'm the second")
+                )
+            ),
+            stdout = TRUE
+        ),
+        "I'm the first I'm the second"
     )
 })
