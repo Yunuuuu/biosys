4ac993e59b53e00e7aada4f82ce038446f4fbeb7
yun <yunyunpp96@outlook.com>
2024-03-10 12:57:49 +0800

    add function `fastq_read_pair`

--- R/exec-fastq_pair.R
+++ R/exec-fastq_pair.R
@@ -18,8 +18,8 @@
 #' controls whether we should remove the uncompressed temporary files.
 #' @param compress A bool, whether compress final results.
 #' @inheritParams allele_counter
-#' @param kraken2 `r rd_cmd("kraken2")`.
-#' @seealso <https://github.com/DerrickWood/kraken2/wiki/Manual>
+#' @param fastq_pair `r rd_cmd("fastq_pair")`.
+#' @seealso <https://github.com/linsalrob/fastq-pair>
 #' @export
 fastq_pair <- exec_build(
     command_new_name("fastq_pair"),
@@ -62,3 +62,63 @@ fastq_pair <- exec_build(
         }
     )
 )
+
+#' @param fastq_files A character of the fastq file path.
+#' @rdname fastq_pair
+#' @export
+fastq_read_pair <- function(fastq_files) {
+    header_list <- lapply(fastq_files, function(file) {
+        if (is_gzip_suffix(file)) {
+            con <- gzfile(file, open = "r")
+        } else if (is_bz2(file)) {
+            con <- bzfile(file, open = "r")
+        } else {
+            con <- file(file, open = "r")
+        }
+        on.exit(close(con))
+        header <- readLines(con, n = 1L)
+        strsplit(header, ":| ", perl = TRUE)[[1L]]
+    })
+    # @HWI-ST1276:71:C1162ACXX:1:1101:1208:2458 2:N:0:CGATGT
+    # HWI-ST1276 <- Unique instrument name
+    # 71 <- Run ID
+    # C1162ACXX <- Flowcell ID
+    # 1 <- Flowcell lane
+    # 1101 <- Tile number within the flowcell lane
+    # 1208 <- 'x'-coordinate of the cluster within the tile
+    # 2458 <- 'y'-coordinate of the cluster within the tile
+
+    # 2 <- Member of a pair,1 or 2 (paired-end or mate-pair reads only)
+    # N <- Y if the read fails filter (read is bad), N otherwise
+    # 0 <- 0 when none of the control bits are on,otherwise it is an even number
+    # CGATGT -> Index sequence
+
+    # In Illumina data, read group IDs are composed using the flowcell name and
+    # lane number, making them a globally unique identifier across all
+    # sequencing data in the world.
+    # Use for BQSR: ID is the lowest denominator that differentiates factors
+    # contributing to technical batch effects: therefore, a read group is
+    # effectively treated as a separate run of the instrument in data processing
+    # steps such as base quality score recalibration (unless you have PU
+    # defined), since they are assumed to share the same error model.
+
+    # https://gatk.broadinstitute.org/hc/en-us/articles/360035890671-Read-groups
+    # https://samtools.github.io/hts-specs/SAMv1.pdf
+    # https://angus.readthedocs.io/en/2017/Read_group_info.html
+    # header <- header_list[[1L]]
+    # id <- sub("^@", "", paste(header[3:4], collapse = "."), perl = TRUE)
+    # flowcell_id <- header[[3L]]
+    # lane_id <- header[[4L]]
+
+    # Platform/technology used to produce the reads. Valid values: CAPILLARY,
+    # DNBSEQ (MGI/BGI), ELEMENT, HELICOS, ILLUMINA, IONTORRENT, LS454, ONT
+    # (Oxford Nanopore), PACBIO (Pacific Bio-sciences), SOLID, and ULTIMA. This
+    # field should be omitted when the technology is not in this list (though
+    # the PM field may still be present in this case) or is unknown
+    # platform_id <- "ILLUMINA"
+
+    # extract pair_id ----------------------------
+    vapply(header_list, function(header) as.integer(header[[8L]]),
+        integer(1L), USE.NAMES = FALSE # styler: off
+    )
+}
